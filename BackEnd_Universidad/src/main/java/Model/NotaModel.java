87baab186eacb808a84e8658cd5433b1022b992e
Yendri2798 <yendri.masis.brenes@est.una.ac.cr>
2022-04-15 11:38:27 -0600

    Modelo y controlador de nota

---
+++ BackEnd_Universidad/src/main/java/Model/NotaModel.java
@@ -0,0 +1,37 @@
+package Model;
+
+import DataBaseConnectionDB.GlobalException;
+import DataBaseConnectionDB.NoDataException;
+import DataBaseConnectionDB.ServicioNota;
+import LogicaNegocio.Nota;
+
+import java.util.List;
+
+public class NotaModel {
+    private static final NotaModel instancia = null;
+    private final ServicioNota servicio;
+
+    private NotaModel() {
+        servicio = new ServicioNota();
+    }
+
+
+    public static NotaModel obtenerInstancia() {
+        return instancia == null ? new NotaModel() : instancia;
+    }
+
+
+    public List<Nota> obtenerNota(String id) throws NoDataException, GlobalException {
+        return servicio.buscarNotasAlumno(id);
+    }
+
+    public void insertarNota(Nota nota) throws NoDataException, GlobalException {
+        System.out.println(nota);
+        servicio.insertarNota(nota);
+    }
+
+    public void modificar(Nota nota) throws NoDataException, GlobalException {
+        servicio.modificaNota(nota);
+    }
+
+}
