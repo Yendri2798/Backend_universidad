3ee8aaf6bb8486a8d1957bcd6e78300426acf375
Bryan Sánchez Brenes <bryan.jsb.1801.@gmail.com>
2022-04-17 16:12:32 -0600

    Update LoginController.java

--- BackEnd_Universidad/src/main/java/Controller/LoginController.java
+++ BackEnd_Universidad/src/main/java/Controller/LoginController.java
@@ -1,9 +1,13 @@
 package Controller;
 
+import DataBaseConnectionDB.GlobalException;
+import DataBaseConnectionDB.NoDataException;
 import LogicaNegocio.Alumno;
 import LogicaNegocio.Login;
+import LogicaNegocio.Profesor;
 import Model.AlumnoModel;
 import Model.LoginModel;
+import Model.ProfesorModel;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -18,49 +22,55 @@ import jakarta.ws.rs.core.Response;
 public class LoginController {
 
 
-    private final LoginModel loginModell = LoginModel.obtenerInstancia();
+    private final LoginModel loginModel = LoginModel.obtenerInstancia();
     private final AlumnoModel alumnoModel = AlumnoModel.obtenerInstancia();
+    private final ProfesorModel profesorModel = ProfesorModel.obtenerInstancia();
 
-    @POST
-    @Path("/verify")
-    public Response verificarUsuario(Login user) {
-        try {
-            boolean a = loginModell.
+    public boolean verificarUsuario(Login user) throws NoDataException, GlobalException {
+
+            boolean a = loginModel.
                     verificarLogin(user.getCedula(), user.getContrasena());
 
-            if (!a) {
-                throw new Exception("Denegado");
-            }
+            return a;
 
-            return Response.ok(a).build();
-        } catch (Exception e) {
-            return Response.status(Response.Status.NOT_ACCEPTABLE)
-                    .entity(e.getMessage()).build();
-        }
     }
 
     @POST
-    @Path("/obtenerTipoUsuario")
-    public Response obtenerTipoUsuario(Login user) {
+    @Path("/obtenerUsuarioEstudiante")
+    public Response obtenerUsuarioEstudiante(Login user) {
+
         try {
+            verificarUsuario(user);
 
-            String ptr = loginModell.obtenerRolLogin(user.getCedula());
-
-
-            return Response.ok(ptr).build();
-        } catch (Exception e) {
+        } catch (NoDataException | GlobalException e) {
             return Response.status(Response.Status.NOT_ACCEPTABLE)
                     .entity(e.getMessage()).build();
         }
-    }
-
-    @POST
-    @Path("/obtenerUsuario")
-    public Response obtenerUsuario(Login user) {
         try {
 
             Alumno alumno = alumnoModel.obtenerAlumno(user.getCedula());
             return Response.ok(alumno).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.NOT_ACCEPTABLE)
+                    .entity(e.getMessage()).build();
+        }
+    }
+
+    @POST
+    @Path("/obtenerUsuarioProfesor")
+    public Response obtenerUsuarioProfesor(Login user) {
+
+        try {
+            verificarUsuario(user);
+
+        } catch (NoDataException | GlobalException e) {
+            return Response.status(Response.Status.NOT_ACCEPTABLE)
+                    .entity(e.getMessage()).build();
+        }
+        try {
+
+            Profesor profesor = profesorModel.obtenerProfesor(user.getCedula());
+            return Response.ok(profesor).build();
         } catch (Exception e) {
             return Response.status(Response.Status.NOT_ACCEPTABLE)
                     .entity(e.getMessage()).build();
